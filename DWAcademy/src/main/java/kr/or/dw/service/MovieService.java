9d75162f363d21b235064a4703da845cf9c611b8
pjkm00 <pjkm00@gmail.com>
2023-07-10 14:40:17 +0900

    메인페이지

--- DWAcademy/src/main/java/kr/or/dw/service/MovieService.java
+++ DWAcademy/src/main/java/kr/or/dw/service/MovieService.java
@@ -1,5 +1,17 @@
 package kr.or.dw.service;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+
 public interface MovieService {
+
+	/**
+	 * 영화예매순으로 10개 가져오는 메서드
+	 * @return
+	 * @throws SQLException
+	 */
+	List<Map<String, Object>> getIndexBoxOfficeMovie10() throws SQLException;
 
 }
